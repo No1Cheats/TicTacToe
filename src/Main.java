f75fd8a20d7df5c6a2bd81afcdae9e04d2e53e1a
No1Cheats <8zgdUV2ALewz>
2019-12-23 17:42:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,121 @@
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer> playerPostions = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ' , '|' , ' ' , '|' , ' '},
+                {'-' , '+' , '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-' , '+' , '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Enter your placement 1-9: ");
+            int playerPos = scan.nextInt();
+            move(gameBoard, playerPos, "player");
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9)+1;
+            move(gameBoard, cpuPos, "cpu");
+            printGameBoard(gameBoard);
+        }
+
+
+
+
+
+
+
+    }
+
+    public static String checkWinner(){
+
+        List topRow = Arrays.asList(1,2,3);
+        List middleRow = Arrays.asList(4,5,6);
+        List bottomRow = Arrays.asList(7,8,9);
+        List leftCol = Arrays.asList(1,4,7);
+        List midCol = Arrays.asList(2,5,8);
+        List rightCol = Arrays.asList(3,6,9);
+        List cross1 = Arrays.asList(1,5,9);
+        List cross2 = Arrays.asList(7,5,3);
+
+        List<List> winningCondition = new ArrayList<List>();
+        winningCondition.add(topRow);
+        winningCondition.add(middleRow);
+        winningCondition.add(bottomRow);
+        winningCondition.add(leftCol);
+        winningCondition.add(midCol);
+        winningCondition.add(rightCol);
+        winningCondition.add(cross1);
+        winningCondition.add(cross2);
+
+        for(List l : winningCondition){
+            if(playerPostions.contains(l)){
+                return "congrats";
+            } else if()
+        }
+
+
+
+
+        return "";
+    }
+
+    public static void printGameBoard(char[][] gameBoard){
+        for(char[] row : gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void move(char[][] gameBoard, int pos, String user){
+
+        char symbol;
+
+        if(user.equals("player")){
+            symbol = 'X';
+        } else {
+            symbol = 'O';
+        }
+
+        switch (pos){
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+        }
+    }
+
+}
